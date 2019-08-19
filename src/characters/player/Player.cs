3cf4931c520e8b58f9d778a48689998f38856441
Callum <callum@sea-jay.net>
2019-08-19 14:08:44 +0100

    Add jumping and falling

--- src/characters/player/Player.cs
+++ src/characters/player/Player.cs
@@ -4,29 +4,31 @@ using System;
 public class Player : KinematicBody2D
 {
     [Export]
-    public float moveSpeed = 16.0f;
-    private Vector2 moveDirection = new Vector2(); 
+    private float movementSpeed = 128.0f;
+    private Vector2 movementDirection = new Vector2();
+    private Vector2 movementVelocity = new Vector2(); 
 
     [Export]
-    public float jumpSpeed = 16.0f;
+    public float jumpSpeed = 256.0f;
+    [Export]
+    public float jumpCoyoteTime = 1.0f;
+    private float coyoteTime = 0.0f;
 
     [Export]
-    public float fallGravity = 1.0f;
+    public float fallGravity = 8.0f;
     [Export]
-    public float fallTerminalVelocity = 32.0f;
+    public float fallTerminalVelocity = 256.0f;
 
-    enum State
+    public enum State
     {
         Normal,
         Airborne
     }
     private State state = State.Normal;
 
-    public override void _Ready()
+    public override void _Process(float delta)
     {
-        base._Ready();
-
-        GD.Print("Hello there!");
+        base._Process(delta);
     }
 
     public override void _PhysicsProcess(float delta)
@@ -36,27 +38,74 @@ public class Player : KinematicBody2D
         switch (state)
         {
             case State.Normal:
-                float inputAxis = 0.0f;
+                HandleMovement();
+                HandleJumping();
 
-                if (Input.IsActionPressed("move_left"))
+                if (!IsOnFloor())
                 {
-                    inputAxis -= 1.0f;
+                    Transition(State.Airborne);
                 }
-                else if (Input.IsActionPressed("move_right"))
+                break;
+            case State.Airborne:
+                HandleMovement();
+                HandleFalling();
+
+                if (IsOnFloor())
                 {
-                    inputAxis += 1.0f;
+                    Transition(State.Normal);
                 }
-
-                if (Input.IsActionJustPressed("move_jump"))
-                {
-                    moveDirection.y -= jumpSpeed;
-                }
-
-                moveDirection.x = inputAxis;
-                moveDirection.y = Mathf.Min(moveDirection.y + fallGravity, fallTerminalVelocity);
-
-                var velocity = MoveAndSlide(moveDirection * moveSpeed);
                 break;
         }
+
+        movementVelocity = MoveAndSlide
+        (
+            movementDirection, // Linear velocity
+            Vector2.Up // Floor normal vector
+        );
+    }
+
+    public void Transition(State nextState)
+    {
+        switch (nextState)
+        {
+            default:
+                break;
+        }
+        
+        state = nextState;
+    }
+
+    private void HandleMovement()
+    {
+        float inputAxis = 0.0f;
+
+        if (Input.IsActionPressed("move_left"))
+        {
+            inputAxis -= 1.0f;
+        }
+        else if (Input.IsActionPressed("move_right"))
+        {
+            inputAxis += 1.0f;
+        }
+
+        movementDirection.x = inputAxis * movementSpeed;
+    }
+
+    private void HandleJumping()
+    {
+        if (Input.IsActionJustPressed("move_jump"))
+        {
+            movementDirection.y = -jumpSpeed;
+        }
+    }
+
+    private void HandleFalling()
+    {
+        if (IsOnCeiling())
+        {
+            movementDirection.y = 0.0f;
+        }
+
+        movementDirection.y = Mathf.Min(movementDirection.y + fallGravity, fallTerminalVelocity);
     }
 }
